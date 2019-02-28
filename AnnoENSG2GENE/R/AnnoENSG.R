acd872eabef5978712f105300b9cdac2e9559df7
ChrisLou-bioinfo <loujiacheng1986@foxmail.com>
2019-02-28 04:11:43 -0500

    v1.0.1 update

--- AnnoENSG2GENE/R/AnnoENSG.R
+++ AnnoENSG2GENE/R/AnnoENSG.R
@@ -1,11 +1,21 @@
-AnnoENSG<-function(data=vsd.expr,datafrom="TCGA"){
+AnnoENSG<-function(Entrez=T,data=vsd.expr,datafrom="TCGA"){
   vsd.expr<-data
-  if (datafrom=="TCGA"){
-    vsd.expr$TCGA.ID<-rownames(vsd.expr)
-    vsd.exprG <- merge(anno.ENSG,vsd.expr,by="TCGA.ID")
-  }else{
-    vsd.expr$ENSG<-rownames(vsd.expr)
-    vsd.exprG <- merge(anno.ENSG,vsd.expr,by="ENSG")
+  if (Entrez==T){ 
+    if (datafrom=="TCGA"){
+      vsd.expr$TCGA.ID<-rownames(vsd.expr)
+      vsd.exprG <- merge(annoENSG.entrzENSG,vsd.expr,by="TCGA.ID")
+    }else{
+      vsd.expr$ENSG<-rownames(vsd.expr)
+      vsd.exprG <- merge(annoENSG.entrzENSG,vsd.expr,by="ENSG")
+    }
+    }else{
+          if (datafrom=="TCGA"){
+              vsd.expr$TCGA.ID<-rownames(vsd.expr)
+              vsd.exprG <- merge(anno.ENSG,vsd.expr,by="TCGA.ID")
+            }else{
+              vsd.expr$ENSG<-rownames(vsd.expr)
+              vsd.exprG <- merge(anno.ENSG,vsd.expr,by="ENSG")
   }
+    }
   return(vsd.exprG)
 }
