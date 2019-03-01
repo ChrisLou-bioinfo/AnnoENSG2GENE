0957d2c7d742b4fc1fdf88d9a5d2901add2a3b3b
ChrisLou-bioinfo <loujiacheng1986@foxmail.com>
2019-03-01 12:15:23 -0500

    1.0.3

--- R/AnnoENSG.R
+++ R/AnnoENSG.R
@@ -1,11 +1,16 @@
-AnnoENSG<-function(EntrzID=T,data=vsd.expr){ 
+AnnoENSG<-function(EntrezID=F,col="rownmanes",data=vsd.expr){ 
   vsd.expr<-data
-  if (EntrzID==T){ 
-    vsd.expr$ID<-stringr::str_sub(rownames(vsd.expr),end=15)
-    vsd.exprG <- merge(annoENSG.entrz,vsd.expr,by="ID")
+  if (EntrezID==T){
+    anno<-annoENSG.entrz
   }else{
+    anno<-anno.ENSG
+  }
+  if (col=="rownames"){
     vsd.expr$ID<-stringr::str_sub(rownames(vsd.expr),end=15)
-    vsd.exprG <- merge(anno.ENSG,vsd.expr,by="ID")
+    vsd.exprG <- merge(anno,vsd.expr,by="ID")
+  }else{
+    vsd.expr$ID<-stringr::str_sub(vsd.expr[,colnames(vsd.expr)==col],end=15)
+    vsd.exprG <- merge(anno,vsd.expr,by="ID")
   }
   return(vsd.exprG)
 }
