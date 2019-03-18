e03bedcb6577d5ee8d2dd986e5afe3f8f25c75e2
ChrisLou-bioinfo <loujiacheng1986@foxmail.com>
2019-03-18 11:38:38 -0400

    1.0.5 add FilterLnc

--- AnnoENSG2GENE/R/AnnoENSG.R
+++ AnnoENSG2GENE/R/AnnoENSG.R
@@ -1,4 +1,4 @@
-AnnoENSG<-function(EntrezID=F,col="rownames",data=vsd.expr){ 
+AnnoENSG<-function(EntrezID=F,col="rownames",data=vsd.expr,FilterLnc=F){ 
   vsd.expr<-data
   if (EntrezID==T){
     anno<-annoENSG.entrz
@@ -6,11 +6,22 @@ AnnoENSG<-function(EntrezID=F,col="rownames",data=vsd.expr){
     anno<-anno.ENSG
   }
   if (col=="rownames"){
-      vsd.expr$ID<-stringr::str_sub(rownames(vsd.expr),end=15)
-      vsd.exprG <- merge(anno,vsd.expr,by="ID")
-    }else{
-      vsd.expr$ID<-stringr::str_sub(vsd.expr[,colnames(vsd.expr)==col],end=15)
-      vsd.exprG <- merge(anno,vsd.expr,by="ID")
+    vsd.expr$ID<-stringr::str_sub(rownames(vsd.expr),end=15)
+    vsd.exprG <- merge(anno,vsd.expr,by="ID")
+  }else{
+    vsd.expr$ID<-stringr::str_sub(vsd.expr[,colnames(vsd.expr)==col],end=15)
+    vsd.exprG <- merge(anno,vsd.expr,by="ID")
+  }
+  if (FilterLnc=T){
+    vsd.exprG<-subset(vsd.exprG,
+                      vsd.exprG$TYPE=="antisense_RNA" | 
+                        vsd.exprG$TYPE=="lincRNA"| 
+                        vsd.exprG$TYPE=="Sense intronic"| 
+                        vsd.exprG$TYPE=="3prime overlapping ncRNA"| 
+                        vsd.exprG$TYPE=="Non coding"| 
+                        vsd.exprG$TYPE=="Retained intron"| 
+                        vsd.exprG$TYPE=="Sense overlapping"| 
+                        vsd.exprG$TYPE=="Macro lncRNA")
   }
   return(vsd.exprG)
 }
--- R/AnnoENSG.R
+++ R/AnnoENSG.R
@@ -1,4 +1,4 @@
-AnnoENSG<-function(EntrezID=F,col="rownames",data=vsd.expr){ 
+AnnoENSG<-function(EntrezID=F,col="rownames",data=vsd.expr,FilterLnc=F){ 
   vsd.expr<-data
   if (EntrezID==T){
     anno<-annoENSG.entrz
@@ -11,6 +11,17 @@ AnnoENSG<-function(EntrezID=F,col="rownames",data=vsd.expr){
   }else{
     vsd.expr$ID<-stringr::str_sub(vsd.expr[,colnames(vsd.expr)==col],end=15)
     vsd.exprG <- merge(anno,vsd.expr,by="ID")
+  }
+  if (FilterLnc=T){
+    vsd.exprG<-subset(vsd.exprG,
+                      vsd.exprG$TYPE=="antisense_RNA" | 
+                        vsd.exprG$TYPE=="lincRNA"| 
+                        vsd.exprG$TYPE=="Sense intronic"| 
+                        vsd.exprG$TYPE=="3prime overlapping ncRNA"| 
+                        vsd.exprG$TYPE=="Non coding"| 
+                        vsd.exprG$TYPE=="Retained intron"| 
+                        vsd.exprG$TYPE=="Sense overlapping"| 
+                        vsd.exprG$TYPE=="Macro lncRNA")
   }
   return(vsd.exprG)
 }
